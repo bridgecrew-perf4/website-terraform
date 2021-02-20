04c8fcd964d0e13319e1f45af826c4c357133092
Riemer van der Zee <riemervdzee@gmail.com>
2021-02-20 21:52:52 +0100

    Add website definition to s3 website definition

--- main.tf
+++ main.tf
@@ -2,4 +2,9 @@ resource "aws_s3_bucket" "website" {
   bucket = "main-website"
   acl    = "public-read"
   tags   = var.default_tags
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
 }
