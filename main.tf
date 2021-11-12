b42f27fa076a0e878021d3b06edd0063a52a3610
Riemer van der Zee <riemervdzee@gmail.com>
2021-11-12 16:55:46 +0100

    Force plan+apply

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_bucket" "website" {
   bucket = "website-${var.name_postfix}"
   acl    = "public-read"
-  tags   = var.default_tags
   policy = data.aws_iam_policy_document.website_policy.json
+
+  tags = var.default_tags
 
   website {
     index_document = "index.html"
